45d0fc8aab0116f94747015933e4aaf04ed5f2ec
Sergey Darchuk <darchuk.sergey@gmail.com>
2023-05-29 01:24:22 +0300

    refresh contact email

--- js/main.js
+++ js/main.js
@@ -43,33 +43,65 @@
     $('.site-main-menu').on("click", "a", function (e) {
         mobileMenuHide();
     });
-    const messageBox = document.querySelector('.form_message_box');
-    document.getElementById('contact-form').addEventListener('submit', function (event) {
-        messageBox.classList.remove('form_message_box--show');
-        messageBox.classList.remove('form_message_box--danger');
-        messageBox.classList.remove('form_message_box--success');
-        const serviceID = 'service_tn38a9h';
-        const templateID = 'template_8blc9lh';
-        messageBox.innerHTML = '';
-        event.preventDefault();
-        emailjs.sendForm(serviceID, templateID, this)
-            .then(response => {
-                // console.log('SUCCESS!', response.status, response.text);
-                messageBox.classList.add('form_message_box--show');
-                messageBox.classList.add('form_message_box--success');
-                messageBox.innerHTML = 'Message sent.';
-                this.reset();
-                setTimeout(function () {
-                    messageBox.classList.remove('form_message_box--show');
-                    messageBox.classList.remove('form_message_box--danger');
-                    messageBox.classList.remove('form_message_box--success');
-                    messageBox.innerHTML = '';
-                }, 3000);
-            }, (error) => {
-                // console.log('FAILED...', error);
-                messageBox.classList.add('form_message_box--show');
-                messageBox.classList.add('form_message_box--danger');
-                messageBox.innerHTML = 'Произошла ошибка.';
-            });
-    });
+    // const messageBox = document.querySelector('.form_message_box');
+    // document.getElementById('contact-form').addEventListener('submit', function (event) {
+    //     messageBox.classList.remove('form_message_box--show');
+    //     messageBox.classList.remove('form_message_box--danger');
+    //     messageBox.classList.remove('form_message_box--success');
+    //     const serviceID = 'service_tn38a9h';
+    //     const templateID = 'template_8blc9lh';
+    //     messageBox.innerHTML = '';
+    //     event.preventDefault();
+    //     emailjs.sendForm(serviceID, templateID, this)
+    //         .then(response => {
+    //             // console.log('SUCCESS!', response.status, response.text);
+    //             messageBox.classList.add('form_message_box--show');
+    //             messageBox.classList.add('form_message_box--success');
+    //             messageBox.innerHTML = 'Message sent.';
+    //             this.reset();
+    //             setTimeout(function () {
+    //                 messageBox.classList.remove('form_message_box--show');
+    //                 messageBox.classList.remove('form_message_box--danger');
+    //                 messageBox.classList.remove('form_message_box--success');
+    //                 messageBox.innerHTML = '';
+    //             }, 3000);
+    //         }, (error) => {
+    //             // console.log('FAILED...', error);
+    //             messageBox.classList.add('form_message_box--show');
+    //             messageBox.classList.add('form_message_box--danger');
+    //             messageBox.innerHTML = 'Произошла ошибка.';
+    //         });
+    // });
+
+    // Отправка заявки 
+$(document).ready(function() {
+	$('#contact-form').submit(function() { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
+		if (document.form.name.value == '' || document.form.email.value == '' ) {
+			valid = false;
+			return valid;
+		}
+		$.ajax({
+			type: "POST",
+			url: "../php/index.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$('.js-overlay-thank-you').fadeIn();
+			$(this).find('input').val('');
+			$('#contact-form').trigger('reset');
+		});
+		return false;
+	});
+});
+
+// Закрыть попап «спасибо»
+// $('.js-close-thank-you').click(function() { // по клику на крестик
+// 	$('.js-overlay-thank-you').fadeOut();
+// });
+
+// $(document).mouseup(function (e) { // по клику вне попапа
+//     var popup = $('.popup');
+//     if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+//         $('.js-overlay-thank-you').fadeOut();
+//     }
+// });
 })(jQuery);
